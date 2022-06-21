b06685d7888e6e19ddf471e787dfc71ad6b9c594
Gabriel Fauth <gabrielfauth@outlook.com>
2022-06-21 14:40:08 -0400

    removendo django heroku

--- bi_oxi/settings.py
+++ bi_oxi/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import django_heroku
+# import django_heroku
 import os
 from pathlib import Path
 
@@ -202,4 +202,4 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
